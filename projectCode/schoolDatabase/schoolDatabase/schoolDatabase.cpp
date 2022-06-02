24f47d27883686c4a31638dde8a8d9ae2ce591dc
Whitehatcat <55483594+cattythegamer-tcat@users.noreply.github.com>
2022-06-02 15:57:23 +1200

    Added school database files and code layout

---
+++ projectCode/schoolDatabase/schoolDatabase/schoolDatabase.cpp
@@ -0,0 +1,33 @@
+// schoolDatabase.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// Includes
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
+// Using namespaces
+using std::cin;
+using std::cout;
+using std::endl;
+
+using std::string;
+using std::vector;
+
+int variableExampleConvention = 0;
+
+// Adds a new parent
+void registerParent() {
+
+}
+
+// Adds a new teacher
+void registerTeacher() {
+
+}
+
+// Main Function
+int main()
+{
+    // Startup Menu
+
+    return; // Placeholder
+}
